ad565bb8d8390b21ee94c9b7a4c28eec9ed3edb8
SpidiMun <78678768+SpidiMun@users.noreply.github.com>
2023-07-25 00:47:10 +0500

    Asymmetric Encryption
    Asymmetric Encryption

---
+++ AsymmetricEncryption/AssymetricEncryption.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Numerics;
+
+namespace AsymmetricEncryption
+{
+    /// <summary>
+    /// батя дал говна
+    /// </summary>
+    internal class AssymetricEncryption
+    {
+        private static readonly Random randomizer = new Random();
+
+        // Для ключа
+        // public key - y, g, p
+        // private key - x
+
+        public BigInteger P { get; set; } // Ввод (простое)
+        public BigInteger G { get; set; } // На основе p > g (простое)
+        public BigInteger X { get; set; } // На основе p > x (простое)
+        public BigInteger Y { get; set; } // y = g^x mod p
+
+        // Для подписи
+
+        public BigInteger K { get; set; } // На основе p - 1 (простое)
+        public BigInteger A { get; set; } // a = g^k mod p
+        public string M { get; set; } // length(M) = (xa + kb) mod (p - 1) (Найти b)
+        public BigInteger B { get; set; } // На основе M
+
+        // подпись - a и b
+        // private - k
+
+        // Верификация: y^a * a^b mod p = g^length(M) mod p
+        // 6k + 1, 6k + 5 (Исключения - 2 и 3)
+        
+        public BigInteger[] ModN { get; set; }
+        public BigInteger N { get; set; }
+        public BigInteger[] exceptions = { 2, 3 };
+
+        public AssymetricEncryption()
+        {
+            N = 6; // Основание модуля
+            ModN = new BigInteger[] { 1, 5 }; // Классы вычета по модулю 6
+        }
+
+        private bool IsPrime(BigInteger number)
+        {
+            if ((number & 1) == 0)
+            {
+                return number == 2;
+            }
+            for (int i = 3; (i * i) <= number; i += 2)
+            {
+                if ((number % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return number != 1;
+        }
+
+        private BigInteger GenerateSimpleNumber(BigInteger MaxNumber)
+        {
+            BigInteger number = 4;
+            while (!IsPrime(number) || number >= MaxNumber)
+            {
+                BigInteger temp1 = N * Convert.ToUInt64(randomizer.Next(100000000, 1000000000)),
+                    temp2 = ModN[randomizer.Next(0, ModN.Length)];
+                number = temp1 + temp2;
+            }
+            return number;
+        }
+
+        public void GenerateKeys()
+        {
+            // public key
+
+            P = GenerateSimpleNumber(1000000000000000ul);
+            G = GenerateSimpleNumber(P);
+            X = GenerateSimpleNumber(P);
+
+            // private key
+
+            Y = BigInteger.Pow(G, (int)X) % P;
+        }
+
+        public void GenerateSignature()
+        {
+            // private signature
+
+            K = GenerateSimpleNumber(P - 1);
+
+            // public signature
+
+            A = BigInteger.Pow(G, (int)K) % P;
+            BigInteger num = BigInteger.Remainder(M.Length - X * A, P - 1);
+            if (num < BigInteger.Zero)
+            {
+                num += P - 1;
+            }
+            for (B = BigInteger.Zero; B < P; B++)
+            {
+                if (BigInteger.Remainder(K * B, P - 1) == num)
+                {
+                    break;
+                }
+            }
+        }
+
+        public bool VerificateSignatureSucsess => BigInteger.Pow(Y, (int)A) * BigInteger.Pow(A, (int)B) % P == BigInteger.Pow(G, M.Length) % P;
+    }
+}
---
+++ AsymmetricEncryption/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Numerics;
+using System.Windows;
+
+namespace AsymmetricEncryption
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly AssymetricEncryption encryptor = new AssymetricEncryption();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckSignatureButton_Click(object sender, RoutedEventArgs e)
+        {
+            statusBar.Text = string.Empty;
+            statusBar.Text = encryptor.P != BigInteger.Zero
+                ? encryptor.VerificateSignatureSucsess ? "Подпись является подлинной!" : "Подпись не является подлинной!"
+                : "Ключ не сгенерирован!";
+        }
+
+        private void KeyGeneratorButton_Click(object sender, RoutedEventArgs e)
+        {
+            statusBar.Text = string.Empty;
+            if (messageTextBox.Text.Length != 0)
+            {
+                encryptor.M = messageTextBox.Text;
+                encryptor.GenerateKeys();
+                encryptor.GenerateSignature();
+                pText.Text = encryptor.P.ToString();
+                gText.Text = encryptor.G.ToString();
+                yText.Text = encryptor.Y.ToString();
+                xText.Text = encryptor.X.ToString();
+                kText.Text = encryptor.K.ToString();
+                aText.Text = encryptor.A.ToString();
+                bText.Text = encryptor.B.ToString();
+            }
+            else
+            {
+                statusBar.Text = "Введите сообщение!";
+            }
+        }
+    }
+}
